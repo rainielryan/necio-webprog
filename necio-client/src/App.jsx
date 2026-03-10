11c444e7625efd52452b5d244aa0f9515b90a8a9
rainielryan <rainnecio04@gmail.com>
2026-03-10 13:02:12 +0800

    lab-act1

---
+++ necio-client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: Rainiel Necio<br />
+          Email: rainnecio04@gmail.com<br />
+          Other Personal Info: <a href="https://github.com/rainielryan/necio-webprog" target="_blank" rel="noopener noreferrer">Github Profile</a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
